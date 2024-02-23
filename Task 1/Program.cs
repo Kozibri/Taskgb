75d15ef43ddc28a989379f444489e6f212d7b3a5
Kozibri <228privet2285@mail.ru>
2024-02-23 13:14:59 +0200

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void Range(int m, int n)
+{
+    if (m == n)
+    {
+        System.Console.WriteLine(m);
+        return;
+    }
+
+    System.Console.Write(m + "," + " ");
+    Range(m + 1, n);
+
+}
+int n = 20;
+int m = 11;
+Range(m, n);
